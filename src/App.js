4812506b3e2b3e3dee4bf088807667af68d4b3e8
David <dvdhyelee@gmail.com>
2022-08-16 02:37:42 -0700

    added github api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import './App.css';
 import Nav from "./Components/Nav";
 import MainBody from "./Components/MainBody";
+import axios from "axios";
 
 function App() {
   const [tab, setTab] = useState(1);
+  const [ghData, setGhData] = useState({});
   const tabSelector = (value) => {
     setTab(value);
   };
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users/dvdhlee', {
+      mode: "no-cors",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then((response) => {
+      let tempobject = {
+        "avatar_url": response.data.avatar_url,
+        "is_hireable": "Hireable",
+        "bio": response.data.bio
+      };
+      setGhData(tempobject)
+    }
+    );
+  }, []);
+
+  console.log(ghData);
 
   return (
     <div className="App">
